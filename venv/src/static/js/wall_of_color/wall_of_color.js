7afc6264963600e08ee888dfc334c14654c56e6a
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-06-22 19:55:33 -0400

    Finished wall of color

---
+++ venv/src/static/js/wall_of_color/wall_of_color.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function() {
+  var boxes = $('.box');
+
+  boxes.on('click', function() {
+    var $self = $(this);
+
+    var number = Math.random()*256;
+
+    var messyHex = number.toString(16);
+
+    var realHex = '#' + messyHex.slice(3,9);
+
+    $self.css('background-color', realHex);
+  })
+})
